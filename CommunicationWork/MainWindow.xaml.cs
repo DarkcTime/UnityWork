34d2d692a67aebdef3d3c25c42f90c53607543b8
Vlad <vlad_chekmarev_01@mail.ru>
2020-10-30 09:28:43 +0300

    first commit

---
+++ CommunicationWork/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CommunicationWork
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public static Object testMain; 
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            SharedClass.SetFrame(this.MainFrame, new Auth());
+            testMain = DataContext; 
+            
+        }
+
+        public void SetTitle(string title)
+        {
+            
+        }       
+
+        public static void SetSize(int height, int width)
+        {
+            
+        }
+
+    }
+}
---
+++ CommunicationWork/SharedClass.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+
+namespace CommunicationWork
+{
+
+    public delegate void SetTitleDelegate(string title);
+
+    public delegate void SetSizeDelegate(int height, int width);
+
+    class SharedClass
+    {
+        public static SetTitleDelegate setTitle{ get; set; }
+
+        public static SetSizeDelegate setSize { get; set; }
+
+        public static void SetSize(Page page)
+        {
+            setPage?.Invoke(page);
+        }
+
+        public static void SetTitle(string title)
+        {
+
+        }
+
+
+
+
+        public static void SetFrame(Frame frame, Page page)
+        {
+            frame.Content = page;
+        }
+
+        public static void OpenNewPage(System.Windows.DependencyObject obj, Page page)
+        {
+            NavigationService service = NavigationService.GetNavigationService(obj);
+            service.Navigate(page);
+        }
+
+
+    }
+}
